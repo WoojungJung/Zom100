a50926df8897f88c140b5ed86e81d3b0257a3953
WoojungJung <512woojung.jung@gmail.com>
2023-10-22 17:51:39 +0900

    GetMapping 템플릿연결 확인

--- src/main/java/com/example/zom100/controller/UserController.java
+++ src/main/java/com/example/zom100/controller/UserController.java
@@ -3,11 +3,17 @@ package com.example.zom100.controller;
 import com.example.zom100.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/user/*")
+@RequestMapping("/zom100user/*")
 public class UserController {
     private final UserService userService;
+    @GetMapping("/login")
+    public String login(){ return "login"; }
+
+    @GetMapping("/list")
+    public String list(){return "list";}
 }
--- src/main/java/com/example/zom100/controller/WishController.java
+++ src/main/java/com/example/zom100/controller/WishController.java
@@ -3,12 +3,13 @@ package com.example.zom100.controller;
 import com.example.zom100.service.UserService;
 import com.example.zom100.service.WishService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/wish/*")
+@RequestMapping("/zom100wish/*")
 public class WishController {
     private final WishService wishService;
     private final UserService userService;
